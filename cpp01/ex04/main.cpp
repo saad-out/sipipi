6aa93857e4bb91ac01a1301cf301474a08b14107
saad-out <outsaad03@gmail.com>
2024-09-10 11:36:55 +0100

    replace and write to file

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+    {
+        std::cerr << "Usage: ./main <filename> <s1> <s2>" << std::endl;
+        return (1);
+    }
+
+    std::string in_filename = av[1];
+    std::string out_filename = in_filename + ".replace";
+
+    std::ifstream ifile (in_filename);
+    if (!ifile.is_open())
+    {
+        std::cerr << "Unexisting file: " << in_filename << std::endl;
+        return (1);
+    }
+    std::ofstream ofile (out_filename);
+    if (!ofile.is_open())
+    {
+        std::cerr << "Error opening: " << out_filename << std::endl;
+        return (1);
+    }
+    std::string s1 = av[2], s2 = av[3];
+    std::string line;
+
+    while (getline(ifile, line))
+    {
+        size_t  line_length = line.length();
+        size_t  str_size = s1.length();
+    
+        std::size_t line_i = 0;
+        std::size_t index = line.find(s1, line_i);
+        while (index != std::string::npos)
+        {
+            ofile << line.substr(line_i, index - line_i);
+            ofile << s2;
+            line_i = (index + str_size);
+            index = line.find(s1, line_i);
+        }
+        if (line_i < line_length)
+            ofile << line.substr(line_i, std::string::npos);
+        ofile << '\n';
+    }
+    ifile.close();
+    ofile.close();
+    return 0;
+}
