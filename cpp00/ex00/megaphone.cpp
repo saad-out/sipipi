e1b1a66925d7d9ae68e130b4bddb458fbf2ced92
saad-out <outsaad03@gmail.com>
2024-09-23 03:17:22 +0100

    unuse strlen()

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,7 +4,7 @@
 int main(int ac, char **av)
 {
     for (int i = 1; i < ac; i++)
-        for (size_t j = 0; j < strlen(av[i]); j++)
+        for (size_t j = 0; av[i][j] != '\0'; j++)
             std::cout << (char)toupper(av[i][j]);
     if (ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
