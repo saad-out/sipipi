fe32ecd9b628d0116f0e35036dc729b05291d853
saad-out <outsaad03@gmail.com>
2024-10-22 05:27:08 +0100

    rb

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -34,5 +34,33 @@ int main()
             std::cout << e.what() << std::endl;
         }
     }
+
+    {
+        // RobotomyRequestForm
+        Bureaucrat b1("b1", 1);
+        Bureaucrat b2("b2", 150);
+        RobotomyRequestForm f1("f1");
+        RobotomyRequestForm f2("f2");
+
+        std::cout << b1 << std::endl;
+        std::cout << b2 << std::endl;
+        std::cout << f1 << std::endl;
+        std::cout << f2 << std::endl;
+
+        try {
+            f1.beSigned(b1);
+            f1.execute(b1);
+            std::cout << "f1 executed" << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+        try {
+            f2.beSigned(b2);
+            f2.execute(b2);
+            std::cout << "f2 executed" << std::endl;
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
 	return (0);
 }
