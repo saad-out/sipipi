6cdfcf570609ade775f7c76b168dad7f7165eab8
saad-out <outsaad03@gmail.com>
2025-03-20 00:52:37 +0000

    using reference casting

--- cpp06/ex02/cast.cpp
+++ cpp06/ex02/cast.cpp
@@ -1,36 +1,58 @@
 #include "cast.hpp"
+#include <typeinfo>
 
-Base *generate(void)
-{
-    srand(time(NULL));
-    int cls = rand() % 3;
-    std::cout << "cls: " << cls << std::endl;
+Base *generate(void) {
+  srand(time(NULL));
+  int cls = rand() % 3;
+  std::cout << "cls number: " << cls << std::endl;
 
-    switch (cls) {
-        case 0:
-            return (dynamic_cast<Base *>(new A()));
-        case 1:
-            return (dynamic_cast<Base *>(new B()));
-        default:
-            return (dynamic_cast<Base *>(new C()));
-    }
-    return (NULL);
+  switch (cls) {
+  case 0:
+    return (dynamic_cast<Base *>(new A()));
+  case 1:
+    return (dynamic_cast<Base *>(new B()));
+  default:
+    return (dynamic_cast<Base *>(new C()));
+  }
+  return (NULL);
 }
 
-void identify(Base *p)
-{
-    if (dynamic_cast<A*>(p) != NULL)
-        std::cout << "A" << std::endl;
-    else if (dynamic_cast<B*>(p) != NULL)
-        std::cout << "B" << std::endl;
-    else if (dynamic_cast<C*>(p) != NULL)
-        std::cout << "C" << std::endl;
-    else
-        std::cout << "Unknown" << std::endl;
+void identify(Base *p) {
+  if (dynamic_cast<A *>(p) != NULL)
+    std::cout << "A" << std::endl;
+  else if (dynamic_cast<B *>(p) != NULL)
+    std::cout << "B" << std::endl;
+  else if (dynamic_cast<C *>(p) != NULL)
+    std::cout << "C" << std::endl;
+  else
+    std::cout << "Unknown" << std::endl;
 }
 
-void identify(Base &p)
-{
-    identify(&p);
-    return ;
+void identify(Base &p) {
+  try {
+    A &a = dynamic_cast<A &>(p);
+    (void)a;
+    std::cout << "A" << std::endl;
+    return;
+  } catch (std::bad_cast &e) {
+    (void)e;
+  }
+  try {
+    B &b = dynamic_cast<B &>(p);
+    (void)b;
+    std::cout << "B" << std::endl;
+    return;
+  } catch (std::bad_cast &e) {
+    (void)e;
+  }
+  try {
+    C &c = dynamic_cast<C &>(p);
+    (void)c;
+    std::cout << "C" << std::endl;
+    return;
+  } catch (std::bad_cast &e) {
+    (void)e;
+  }
+  std::cout << "Unknown" << std::endl;
+  return;
 }
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
 #include "cast.hpp"
-/*#include "Base.hpp"*/
-/*#include "A.hpp"*/
-/*#include "B.hpp"*/
-/*#include "C.hpp"*/
+#include <iostream>
 
-int main()
-{
-    Base *b = generate();
-    identify(b);
-    delete b;
+int main() {
+  Base *b = generate();
+  identify(b);
+  delete b;
 
-    A a;
-    identify(a);
-    B b2;
-    identify(b2);
-    C c;
-    identify(c);
+  A a;
+  identify(a);
+  B b2;
+  identify(b2);
+  C c;
+  identify(c);
 
-    return (0);
+  return (0);
 }
