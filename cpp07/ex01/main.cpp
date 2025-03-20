d2d4e3158dbf4dc9ff2217509f5fde6f39f9fd49
saad-out <outsaad03@gmail.com>
2025-03-20 22:03:01 +0000

    refactor test

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,12 +1,13 @@
 #include "iter.hpp"
 
-int main()
-{
-    int intArr[5] = {-1, 0, 1, 2, 3};
-    iter(intArr, 5, print);
+int main() {
+  int intArr[5] = {-1, 0, 1, 2, 3};
+  std::cout << "Printing int array" << std::endl;
+  iter(intArr, 5, print);
 
-    char name[5] = {"hala"};
-    iter(name, 5, print);
+  char name[5] = {"hala"};
+  std::cout << "Printing char array" << std::endl;
+  iter(name, 4, print);
 
-    return (0);
+  return (0);
 }
