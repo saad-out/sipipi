7d6328cbc91dbd074ee7352019985f7daa671939
saad-out <outsaad03@gmail.com>
2025-03-20 00:39:03 +0000

    simplify assignement constructor

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -2,23 +2,16 @@
 
 Serializer::Serializer() {}
 Serializer::~Serializer() {}
-Serializer::Serializer(const Serializer& other)
-{
-    *this = other;
-}
-Serializer& Serializer::operator=(const Serializer& other)
-{
-    if (this != &other)
-        return (*this);
-    return (*this);
+Serializer::Serializer(const Serializer &other) { *this = other; }
+Serializer &Serializer::operator=(const Serializer &other) {
+  (void)other;
+  return (*this);
 }
 
-uintptr_t Serializer::serialize(Data* ptr)
-{
-    return (reinterpret_cast<uintptr_t>(ptr));
+uintptr_t Serializer::serialize(Data *ptr) {
+  return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-Data* Serializer::deserialize(uintptr_t raw)
-{
-    return (reinterpret_cast<Data*>(raw));
+Data *Serializer::deserialize(uintptr_t raw) {
+  return (reinterpret_cast<Data *>(raw));
 }
