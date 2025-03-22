55d3eb6f9dc0c70b7dad14cff09c044f2121845c
saad-out <outsaad03@gmail.com>
2025-03-22 17:55:48 +0000

    const reference

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -4,16 +4,14 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* array, unsigned int size, void (*func) (T&))
+void iter(T *array, unsigned int size, void (*func)(T const &))
 {
-    for (unsigned int i = 0; i < size; i++)
-        func(array[i]);
+  for (unsigned int i = 0; i < size; i++)
+    func(array[i]);
 }
 
-template <typename T>
-void print(T& t)
-{
-    std::cout << "=> " << t << std::endl;
+template <typename T> void print(T const &t) {
+  std::cout << "=> " << t << std::endl;
 }
 
 #endif // __ITER_HPP__
