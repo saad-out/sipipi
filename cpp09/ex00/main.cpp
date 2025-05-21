b84ad64df75ae78a485fbed2f69da9f935bddfae
saad-out <outsaad03@gmail.com>
2025-05-21 09:24:40 +0100

    catch exceptions

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -5,8 +5,13 @@ int main(int ac, char **av)
 {
     if (ac != 2)
         return (std::cerr << "Usage: ./btc <filename>" << std::endl, 1);
-    BitCoinExchange btc(av[1]);
-    if (btc.readFile() == -1)
-        return (std::cerr << "Error: Invalid file format." << std::endl, 1);
-    return (0);
+    try {
+      BitCoinExchange btc(av[1]);
+      if (btc.readFile() == -1)
+          return (std::cerr << "Error: Invalid file format." << std::endl, 1);
+      return (0);
+    } catch (const std::exception &e) {
+      std::cerr << "Error: " << e.what() << std::endl;
+      return (1);
+    }
 }
