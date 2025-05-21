3d052ecb0d340e740adcf4e8dc24a9f947855b3b
saad-out <outsaad03@gmail.com>
2025-05-21 09:43:46 +0100

    rewrite valid number checking logic

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -3,68 +3,72 @@
 RPN::RPN() {}
 RPN::~RPN() {}
 RPN::RPN(const RPN &src) : _stack(src._stack) {}
-RPN &RPN::operator=(const RPN &src)
-{
-    if (this != &src)
-        _stack = src._stack;
-    return *this;
+RPN &RPN::operator=(const RPN &src) {
+  if (this != &src)
+    _stack = src._stack;
+  return *this;
 }
 
-int stringToInt(const std::string &str)
-{
-    std::istringstream iss(str);
-    int num;
-    iss >> num;
-    return num;
+int stringToInt(const std::string &str) {
+  std::istringstream iss(str);
+  int num;
+  iss >> num;
+  return num;
 }
 
-int RPN::calculate(const std::string &expression)
-{
-    std::istringstream iss(expression);
-    std::string token;
-    while (iss >> token)
-    {
-        if (isdigit(token[0]))
-            _stack.push(stringToInt(token));
-        else if (isOperator(token))
-            performOperation(token);
-        else
-            throw std::runtime_error("Error: Invalid token");
-    }
-    if (_stack.size() != 1)
-        throw std::runtime_error("Error: Invalid expression");
-    return _stack.top();
+bool isValidDigit(const std::string &str) {
+  if (str.empty())
+    return false;
+  for (size_t i = 0; i < str.length(); i++) {
+    if (str[i] == '+' && i == 0 && str.length() > 1)
+      continue;
+    if (!isdigit(str[i]))
+      return false;
+  }
+  return true;
 }
 
-void RPN::performOperation(const std::string &operation)
-{
-    if (_stack.size() < 2)
-        throw std::runtime_error("Error: Not enough operands");
-    int b = _stack.top();
-    _stack.pop();
-    int a = _stack.top();
-    _stack.pop();
-    _stack.push(applyOperation(a, b, operation));
+int RPN::calculate(const std::string &expression) {
+  std::istringstream iss(expression);
+  std::string token;
+  while (iss >> token) {
+    if (isValidDigit(token))
+      _stack.push(stringToInt(token));
+    else if (isOperator(token))
+      performOperation(token);
+    else
+      throw std::runtime_error("Error: Invalid token");
+  }
+  if (_stack.size() != 1)
+    throw std::runtime_error("Error: Invalid expression");
+  return _stack.top();
 }
 
-bool RPN::isOperator(const std::string &token)
-{
-    return (token == "+" || token == "-" || token == "*" || token == "/");
+void RPN::performOperation(const std::string &operation) {
+  if (_stack.size() < 2)
+    throw std::runtime_error("Error: Not enough operands");
+  int b = _stack.top();
+  _stack.pop();
+  int a = _stack.top();
+  _stack.pop();
+  _stack.push(applyOperation(a, b, operation));
 }
 
-int RPN::applyOperation(int a, int b, const std::string &operation)
-{
-    if (operation == "+")
-        return a + b;
-    else if (operation == "-")
-        return a - b;
-    else if (operation == "*")
-        return a * b;
-    else if (operation == "/")
-    {
-        if (b == 0)
-            throw std::runtime_error("Error: Division by zero");
-        return a / b;
-    }
-    throw std::runtime_error("Error: Invalid operator");
+bool RPN::isOperator(const std::string &token) {
+  return (token == "+" || token == "-" || token == "*" || token == "/");
+}
+
+int RPN::applyOperation(int a, int b, const std::string &operation) {
+  if (operation == "+")
+    return a + b;
+  else if (operation == "-")
+    return a - b;
+  else if (operation == "*")
+    return a * b;
+  else if (operation == "/") {
+    if (b == 0)
+      throw std::runtime_error("Error: Division by zero");
+    return a / b;
+  }
+  throw std::runtime_error("Error: Invalid operator");
 }
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,26 +1,18 @@
-#include <iostream>
 #include "RPN.hpp"
+#include <iostream>
 
-
-int main(int ac, char **av)
-{
-    if (ac == 2)
-    {
-        RPN rpn;
-        try
-        {
-            std::cout << rpn.calculate(av[1]) << std::endl;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << e.what() << std::endl;
-            return 1;
-        }
+int main(int ac, char **av) {
+  if (ac == 2) {
+    RPN rpn;
+    try {
+      std::cout << rpn.calculate(av[1]) << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+      return 1;
     }
-    else
-    {
-        std::cerr << "Error: Invalid number of arguments" << std::endl;
-        return (1);
-    }
-    return (0);
+  } else {
+    std::cerr << "Error: Invalid number of arguments" << std::endl;
+    return (1);
+  }
+  return (0);
 }
